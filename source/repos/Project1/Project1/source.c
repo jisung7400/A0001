d4fe754c873930db7b5c05069733e3ca736442de
jisung7400 <01051607400w@gmail.com>
2025-12-24 21:44:51 +0900

    2025*12*24

--- source/repos/Project1/Project1/source.c
+++ source/repos/Project1/Project1/source.c
@@ -3,122 +3,45 @@
 
 void main()
 {
-#pragma region 주소연산자
-	// 변수의 주소 값을 반환하는 연산자입니다.
+#pragma region 배열
+	// 같은 자료형의 변수들로 이루어진 유한 집합입니다.
 
-	//int data = 10;
+	//int list[5];
 	//
-	//printf("data 변수의 주소 : %p", & data);
+	//// 배열의 경우 첫 번째 원소는 0부터 시작합니다.
 	//
-	// 데이터의 주소는 해당 데이터가 저장된 메모리의 시작 주소를 
-	// 의미하며, 메모리의 공간은 1byte의 크기로 나누어 표현합니다.
-#pragma endregion
+	//list[0] = 10;
+	//list[1] = 20;
+	//list[2] = 30;
+	//list[3] = 40;
+	//list[4] = 50;
+	//
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	list[i] = (i + 1) * 10;
+	//
+	//	printf("list[%d] : %d\n", i, list[i]);
+	//}
 
-#pragma region 표준 입력 함수
-	// 여러 종류의 데이터를 다양한 서식에 맞추어 
-	// 입력해주는 함수입니다.
-
-	//int x = 0;
-
-	// 표준 입력 함수는 입력을 수행할 때까지 다음 작업으로
-	// 넘어갈 수 없습니다.
-
-	//printf("x 변수의 값을 입력 : ");
-
-	//scanf_s("%d", &x);
-
-	// 버퍼는 데이터가 이동할 때 임시로 저장되는 공간이며,
-	// 서식 지정자에 따라 입력할 수 있는 데이터의 범위도 결정됩니다.
-
-	//printf("x의 값 : %d\n", x);
-
-	// 표준 입력 함수로 데이터를 입력하게 되면 버퍼에 데이터를 보관
-	// 하였다가 입력하는 순간 버퍼 안의 내용을 프로그램에 전송합니다.
-#pragma endregion
-
-#pragma region 포인터
-	// 메모리의 주소 값을 저장할 수 있는 변수입니다.
-	
-
-//	int storage = 10;
-//
-//	int room = 20;
-//
-//	int * pointer = &storage;
-//
-//	// 포인터가 가리키는 메모리 공간의 자료형은 알 수 없으므로,
-//	// 포인터가 가리키는 메모리의 자료형을 선언해주어야 합니다.
-//
-//	printf("storage의 주소 : %p\n", &storage);
-//	printf("pointer의 값 : %p\n", pointer);
-//	printf("pointer의 주소 : %p\n", &pointer);
-//
-//	*pointer = 99;
-//
-//	printf("storage의 값 : %d\n", storage);
-//	printf("pointer변수가 가리키는 값 : %d\n", *pointer);
-//
-//	pointer = &room;
-//
-//	printf("pointer의 값 : %p\n", pointer);
-//	printf("pointer변수가 가리키는 값 : %d\n", *pointer);
-//
-//	// 포인터 변수도 자신의 메모리 공간을 가지고 있으며, 포인터 변수에
-//	// 변수의 주소를 저장하게 되면 해당 변수의 시작 주소를 가리킵니다.
-//
-//	printf("int의 크기 : %u\n", sizeof(int));
-//
-//	printf("pointer 변수의 크기 : %u\n", sizeof(pointer));
-
-	// 포인터 변수의 크기는 중앙 처리 장치가 한 번에 처리할 수 있는
-	// 크키로 정해지며, 한 번에 처리할 수 있는 크기는 운영체재에 따라 크기가 결정됩니다.
-
-#pragma endregion
-
-#pragma region 범용 포인터
-	// 자료형이 정해지지 않은 상태로 모든 자료형을
-	// 저장할 수 있는 포인터입니다.
-	
-	char rank = 'A';
-	int count = 20;
-	float angle = 8.125f;
-
-	void * reference = NULL;
-
-	reference = &angle;
-
-	// 범용 포인터는 메모리 주소에 접근해서 값을
+	// 배열의 메모리 공간은 프로그램이 실행되는 동안 
 	// 변경할 수 없습니다.
-	*(char*)reference = "B";
 
-	printf("rank 변수의 값 : %c\n", rank);
-	printf("reference 변수의 값 : %p\n\n", reference);
+	int array[] = { 1, 2, 3, 4, 5 };
 
-	reference = &count;
+	int size = sizeof(array) / sizeof(array[0]);
 
-	*(int*)reference = 50;
-
-	printf("count 변수의 값 : %d\n", count);
-	printf("reference 변수의 값 : %pn\n\n", reference);
-
-	reference = &angle; 
-
-	*(float*)reference = 8.125f;
-
-	printf("angle 변수의 값 : %f\n", angle);
-	printf("reference 변수의 값 : %p\n\n", reference);
-
-	// 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가
-	// 가리키는 변수의 자료형으로 변환해주어야 합니다.
+	for(int i = 0; i < size; i++)
+	{
+		printf("array[%d]의 값 : %d\n", i, array[i]);
+	}
+	// 배열의 크기는 생략할 수 있으며, 초기화 목록에서
+	// 설정한 요소에 따라 배열의 크기가 결정됩니다.
 
 #pragma endregion
 
-#pragma region 상수 지시 포인터
-	// 포인터가 가리키는 주소에 저장되어 있는 값을 변경할 수 없도록
-	// 지정되어 있는 포인터입니다.
+#pragma region 
 
 #pragma endregion
-
 
 
 
